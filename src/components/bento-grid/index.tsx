3022e55ae147837092aeb61e9c977a7d2b869b04
subhash131 <subhashnayak131@gmail.com>
2024-10-15 07:56:45 +0530

    update grid css

--- src/components/bento-grid/index.tsx
+++ src/components/bento-grid/index.tsx
@@ -1,30 +1,48 @@
 import React from "react";
 import style from "./bento.module.css";
 import Rocket from "./rocket";
+import { Sparkle } from "lucide-react";
 
 const BentoGrid = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className={`${style.parent} w-[80%] h-[80%] gap-3`}>
         <div
-          className={`${style.div1} border rounded-2xl border-light overflow-hidden`}
+          className={`${style.div1} border rounded-2xl border-light overflow-hidden hover:scale-95 transition-transform`}
         >
           <Rocket />
         </div>
-        <div className={`${style.div2} border rounded-2xl border-light`}> </div>
-        <div className={`${style.div3} border rounded-2xl border-light `}>
-          {" "}
-        </div>
-        <div className={`${style.div4} border rounded-2xl border-light `}>
-          {" "}
-        </div>
-        <div className={`${style.div5} border rounded-2xl border-light`}> </div>
         <div
-          className={`${style.div6} border rounded-2xl border-light bg-purple`}
+          className={`${style.div2} border rounded-2xl border-light p-2 bg-[#111111] hover:scale-95 transition-transform`}
+        >
+          hello
+        </div>
+        <div
+          className={`${style.div3} border rounded-2xl border-light bg-[#111111] hover:scale-95 transition-transform`}
         >
           {" "}
         </div>
-        <div className={`${style.div7} border rounded-2xl border-light `}>
+        <div
+          className={`${style.div4} border rounded-2xl border-light bg-[#111111] hover:scale-95 transition-transform`}
+        >
+          {" "}
+        </div>
+        <div
+          className={`${style.div5} border rounded-2xl border-light bg-[#111111] hover:scale-95 transition-transform`}
+        >
+          {" "}
+        </div>
+        <div
+          className={`${style.div6} border rounded-2xl border-light bg-purple flex flex-col items-center hover:scale-95 transition-transform`}
+        >
+          <div className="w-full flex justify-between items-center p-4">
+            <h3 className="text-4xl font-semibold">Skills</h3>
+            <Sparkle fill="#DCD4DF" size={28} />
+          </div>
+        </div>
+        <div
+          className={`${style.div7} border rounded-2xl border-light bg-[#111111] hover:scale-95 transition-transform`}
+        >
           {" "}
         </div>
       </div>
--- src/components/bento-grid/rocket.tsx
+++ src/components/bento-grid/rocket.tsx
@@ -2,8 +2,8 @@ import Spline from "@splinetool/react-spline/next";
 
 export default function Rocket() {
   return (
-    <main className="size-full relative">
-      <div className="absolute bottom-0 right-0 h-16 w-40 bg-black"></div>
+    <main className="size-full relative bg-[#111111]">
+      <div className="absolute bottom-0 right-0 h-16 w-40 bg-[#111111]"></div>
       <Spline scene="https://prod.spline.design/cBhuUS0Bc6GVRgJl/scene.splinecode" />
     </main>
   );
